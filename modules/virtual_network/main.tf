ec2fd39e065a4d4273cb5656a362b07b2f3e384f
Wellington Ozorio <well.ozorio@gmail.com>
2021-04-21 14:34:41 -0300

    Change value of resource_group_name from var.name to var.resource_group_name

--- modules/virtual_network/main.tf
+++ modules/virtual_network/main.tf
@@ -2,5 +2,5 @@ resource "azurerm_virtual_network" "vnet" {
   name                = var.name
   address_space       = var.address_space
   location            = var.location
-  resource_group_name = var.name
+  resource_group_name = var.resource_group_name
 }
