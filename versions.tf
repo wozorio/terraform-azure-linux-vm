2fcedda3603a942ff40327f4717b2a5bd87f58fa
Wellington Ozorio <well.ozorio@gmail.com>
2021-04-21 08:14:55 -0300

    Remove empty lines

--- versions.tf
+++ versions.tf
@@ -1,11 +1,10 @@
-
 terraform {
   required_version = ">= 0.14"
   required_providers {
     # https://github.com/terraform-providers/terraform-provider-azurerm/blob/master/CHANGELOG.md
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.52.0"
+      version = "2.56.0"
     }
   }
 }
