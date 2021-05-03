f466f763ce4ddc26cbf465e88888068b68a0100d
Wellington Ozorio <well.ozorio@gmail.com>
2021-05-03 16:51:30 -0300

    Add outputs.tf

---
+++ modules/linux_virtual_machine/outputs.tf
@@ -0,0 +1,11 @@
+output "name" {
+  value = azurerm_linux_virtual_machine.vm.name
+}
+
+output "resource_group_name" {
+  value = azurerm_linux_virtual_machine.vm.resource_group_name
+}
+
+output "location" {
+  value = azurerm_linux_virtual_machine.vm.location
+}
