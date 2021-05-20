b573f830776d67d637571ee6e08f5c7ccf3e8685
Wellington Ozorio <well.ozorio@gmail.com>
2021-05-20 14:55:20 -0300

    remove admin_password input variable

--- variables.tf
+++ variables.tf
@@ -2,9 +2,3 @@ variable "location" {
   description = "The Azure Region where the resources should exist"
   type        = string
 }
-
-variable "admin_password" {
-  description = "The Password which should be used for the local-administrator on the Virtual Machine"
-  type        = string
-  sensitive   = true
-}
