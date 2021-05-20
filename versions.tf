7d611ad88f74b22dc26cab5fb7a1c3e151051c7f
Wellington Ozorio <well.ozorio@gmail.com>
2021-05-20 14:55:42 -0300

    bump Terraform to version 0.15

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 0.15"
   required_providers {
     # https://github.com/terraform-providers/terraform-provider-azurerm/blob/master/CHANGELOG.md
     azurerm = {
