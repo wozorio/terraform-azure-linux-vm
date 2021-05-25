a9aab5f45574d7da08ba548f160dc83eaaaa0d1b
Wellington Ozorio <well.ozorio@gmail.com>
2021-05-25 11:58:45 -0300

    add tfstate extension

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "stterraformwozorio"
     container_name       = "tfstate"
-    key                  = "playground"
+    key                  = "playground.tfstate"
   }
 }
 
