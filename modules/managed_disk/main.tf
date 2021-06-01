ee270fb4a6b2d42ac7985b9d8e11f5cfd4f69b3d
Wellington Ozorio <well.ozorio@gmail.com>
2021-06-01 14:08:49 -0300

    enable disk encryption

--- modules/managed_disk/main.tf
+++ modules/managed_disk/main.tf
@@ -5,4 +5,8 @@ resource "azurerm_managed_disk" "disk" {
   storage_account_type = var.storage_account_type
   create_option        = var.create_option
   disk_size_gb         = var.disk_size_gb
+
+  encryption_settings {
+    enabled = true
+  }
 }
