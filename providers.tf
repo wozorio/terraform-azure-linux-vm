330075c2c22093b5f4daa6839f6ebb1e3a3cb4e7
Wellington Ozorio <well.ozorio@gmail.com>
2024-12-05 11:08:47 -0300

    asdf

--- providers.tf
+++ providers.tf
@@ -1,9 +1,5 @@
 terraform {
-  backend "azurerm" {
-    storage_account_name = "stterraformwozorio"
-    container_name       = "tfstate"
-    key                  = "playground.tfstate"
-  }
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
